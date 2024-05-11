bf26574f869b052e536aa249c7a56cf9b9452c33
RawatRahul14 <rawatrahul1604@gmail.com>
2024-05-11 19:42:11 -0400

    folder structure added

--- template.py
+++ template.py
@@ -4,9 +4,7 @@ import logging
 
 logging.basicConfig(level=logging.INFO, format='[%(asctime)s]: %(message)s:')
 
-
 project_name = "mlProject"
-
 
 list_of_files = [
     ".github/workflows/.gitkeep",
