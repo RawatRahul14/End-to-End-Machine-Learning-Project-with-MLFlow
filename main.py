2655ff347dc49132791f8dd5716e83792505f6cc
RawatRahul14 <rawatrahul1604@gmail.com>
2024-05-14 20:26:35 -0400

    model trainer added

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ from mlProject import logger
 from mlProject.pipeline.stage_01_data_ingestion import DataIngestionTrainingPipeline
 from mlProject.pipeline.stage_02_data_validation import DataValidationTrainingPipeline
 from mlProject.pipeline.stage_03_data_transformation import DataTransformationTrainingPipeline
+from mlProject.pipeline.stage_04_model_trainer import ModelTrainerTrainingPipeline
 
 STAGE_NAME = "Data ingestion stage"
 try:
@@ -29,6 +30,16 @@ STAGE_NAME = "Data transformation stage"
 try:
     logger.info(f">>>>>> stage: {STAGE_NAME} started <<<<<<")
     obj = DataTransformationTrainingPipeline()
+    obj.main()
+    logger.info(f">>>>>> stage: {STAGE_NAME} completed <<<<<<")
+except Exception as e:
+    logger.exception(e)
+    raise e
+
+STAGE_NAME = "Model trainer stage"
+try:
+    logger.info(f">>>>>> stage: {STAGE_NAME} started <<<<<<")
+    obj = ModelTrainerTrainingPipeline()
     obj.main()
     logger.info(f">>>>>> stage: {STAGE_NAME} completed <<<<<<")
 except Exception as e:
